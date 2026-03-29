c0f21ca45e0706ae526f986988b02eab45780137
st10449059 <160851453+st10449059@users.noreply.github.com>
2026-03-29 18:23:48 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PROG6221_Assigment_Part1_ST10449059
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
