db1a63218fae200d518750d61a0ef7465acc6b01
st10449059 <160851453+st10449059@users.noreply.github.com>
2026-03-29 18:38:23 +0200

    Added systems to let the program play the wav file.

--- Chatbot.cs
+++ Chatbot.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Media; // Let's us play the WAV file (Reference: Microsoft, 2024)
+using System.Threading; // Gives us control over the 'typing' speed (Reference: Microsoft, 2024)
 
 namespace PROG6221_Assigment_Part1_ST10449059
 {
